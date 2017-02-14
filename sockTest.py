be3547f8a2877d5c227c32d80c237b56249e25be
SettNozz <settnozz@yandex.ua>
2017-02-14 18:48:41 +0200

    add send picture to stream

--- sockTest.py
+++ sockTest.py
@@ -3,22 +3,27 @@
 
 import socket
 import cv2
-from PIL import Image
+#from PIL import Image
 
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('10.0.0.130', 9090))
+sock.bind(('0.0.0.0', 9090))
 sock.setblocking(False)
 sock.listen(1)
-
+fileName = 'newImage.jpg'
 
 def maybeNewImage():
     try:
         conn, addr = sock.accept()
     except:
-        return None
+        dataTime = 100000
+        return None, dataTime
     conn.setblocking(True)
-    data = conn.recv(4096)
-    imgRet = cv2.imread(data)
+    data = conn.recv(20000)
+    newFile = open(fileName, 'w')
+    newFile.write(data)
+    newFile.close()
+    imgRet = cv2.imread(fileName)
+    dataTime = conn.recv(2048)
     conn.close()
-    return(imgRet)
+    return(imgRet, dataTime)
--- stream.py
+++ stream.py
@@ -1,6 +1,6 @@
 import cv2
 import sockTest
-import subprocess
+#import subprocess
 import os
 
 cam2 = 'rtmp://localhost/myapp/mystream'
@@ -11,30 +11,29 @@ overlay = None
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 out = cv2.VideoWriter('outstream.avi',fourcc, 20.0, (1092, 614))
 counter = 0
+countStop = 0
 
 while(cap.isOpened()):
     counter +=1
     if counter == 10:
-        pass
-        #os.system("gnome-terminal -e 'bash -c \"ffmpeg -re -i /home/settnozz/python_stream/outstream.avi -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/strAds/mystreama; exec bash\"'")
+        print('cadr')
+        os.system("gnome-terminal -e 'bash -c \"ffmpeg -re -i /home/settnozz/python_stream/outstream.avi -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://live.twitch.tv/settnozz/live_64609648_vOa0jBYpxpYNbE0HoXjlf8ENxNDwLK; exec bash\"'")
         #subprocess.call(['ffmpeg', '-re', '-i', '/home/settnozz/python_stream/outstream.avi', '-c:v', 'libx264', '-preset', 'veryfast', '-maxrate', '3000k', '-bufsize', '6000k', '-pix_fmt', 'yuv420p', '-g', '50', '-c:a', 'aac', '-b:a', '160k', '-ac', '2', '-ar', '44100', '-f', 'flv', 'rtmp://localhost/strAds/mystreama'])
     ret, frame = cap.read()
     if frame is None:
         continue
 
 
-    maybeImage = sockTest.maybeNewImage()
+    maybeImage, countStop = sockTest.maybeNewImage()
     if maybeImage is not None:
         overlay = maybeImage
-
-
-
-    if overlay != None:
+        counter = 0
+        print(countStop)
+    if overlay != None and counter < countStop:
         h2, w2 = overlay.shape[:2]
-        frame[:h2, :w2, :] = overlay
+        frame[100:h2 + 100, 100:w2 + 100, :] = overlay
     out.write(frame)
     cv2.imshow('frame', frame)
-    #sys.stdout.write( frame.tostring() )
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
